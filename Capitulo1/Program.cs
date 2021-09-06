0f76d327eabee01718ee6ded90a3e10b8cf8ffcd
Perla D. Canario Perez <perladenis163@gmail.com>
2021-09-06 03:08:03 -0400

    Agregar archivos de proyecto.

---
+++ Capitulo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Capitulo1
+{
+    class Program
+    {
+        public static void Imprimir()
+        {
+            Console.WriteLine("Perla Denis Canario Perez");
+        }
+        public static void OtrosDatos()
+        {
+            Imprimir();
+            Console.WriteLine("Ocupacion: Estudiante de Sistemas \nMatricula: 2019-0008");
+        }
+        static void Main(string[] args)
+        {
+            OtrosDatos();
+        }
+    }
+}
